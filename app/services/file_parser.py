9992b2825e3b243bd26bdac4a1538e7a0264a8ed
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-27 15:23:30 +0300

    [ENH] Added Page Number Detection

--- app/services/file_parser.py
+++ app/services/file_parser.py
@@ -14,20 +14,30 @@ class PDFTextExtractor(TextExtractorStrategy):
     def extract_text(self, pdf_path):
         document = fitz.open(pdf_path)
         text = ""
-        fonts = defaultdict(int)
         for page_num in range(len(document)):
             page = document.load_page(page_num)
             text += page.get_text("text")
-        return text
-    
+        num_pages = len(document)
+        return text, num_pages
+
 # Concrete Strategy for DOCX
 class DOCXTextExtractor(TextExtractorStrategy):
     def extract_text(self, docx_path):
         doc = docx.Document(docx_path)
+        num_pages = get_docx_page_count(docx_path)
         text = ""
+        fonts = defaultdict(int)
         for paragraph in doc.paragraphs:
             text += paragraph.text + "\n"
-        return text
+        fonts = format_fonts(dict(fonts))
+        return text, num_pages
+    
+    @staticmethod
+    def get_docx_page_count(docx_fpath):
+        docx_object = zipfile.ZipFile(docx_fpath)
+        docx_property_file_data = docx_object.read('docProps/app.xml').decode()
+        page_count = re.search(r"<Pages>(\d+)</Pages>", docx_property_file_data).group(1)
+        return int(page_count)
 
 # Context class
 class TextExtractor:
